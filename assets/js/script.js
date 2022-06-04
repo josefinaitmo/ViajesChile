72edf23a9b2ea23d7ce037cb8362a7287bda67c3
josefina <josefinaitmo@gmail.com>
2022-06-03 22:06:32 -0400

    tooltips

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,8 @@ $(document).scroll(function(){
     ("nav-black")
 
 })
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+}); 
